e7251d96bb504cefb7eb195fb9d51e4f8d08b071
V-Holodov <w.a.holodov@yandex.ru>
2021-05-04 12:41:02 +0300

    Add model for follow

--- foodgram/users/models.py
+++ foodgram/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Follow(models.Model):
+
+    """the follow system on the author"""
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='follower')
+    idol = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='mentor')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'idol'],
+                name='unique_follow'
+                )
+        ]
