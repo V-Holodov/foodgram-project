bdfe21baf228e2ceaf7d0c7f0273ae41582a7c62
V-Holodov <w.a.holodov@yandex.ru>
2021-05-05 23:20:52 +0300

    load static for indexAuth

--- foodgram/config/urls.py
+++ foodgram/config/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', include('recipes.urls')),
@@ -22,3 +25,11 @@ urlpatterns = [
     path("auth/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+        )
