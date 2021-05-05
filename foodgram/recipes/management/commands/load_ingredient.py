a238724c9014c7c14e712d37539a4772b86e4ff9
V-Holodov <w.a.holodov@yandex.ru>
2021-05-05 16:04:06 +0300

    WIP manegement comand

---
+++ foodgram/recipes/management/commands/load_ingredient.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+import csv
+
+
+class Command(BaseCommand):
+    help = 'Load ingredient'
+
+    def handle(self, *args, **kwargs)
