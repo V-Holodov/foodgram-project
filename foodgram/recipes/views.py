06c7f5201b601423b1f1a0664d33946627ba606b
V-Holodov <w.a.holodov@yandex.ru>
2021-05-06 23:20:25 +0300

    WIP paginator

--- foodgram/recipes/views.py
+++ foodgram/recipes/views.py
@@ -1,7 +1,11 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from . import models
 
 
 def index(request):
     recipes = models.Recipe.objects.all()
-    return render(request, "index.html", {"recipes": recipes})
+    paginator = Paginator(recipes, 6)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, "index.html", {"recipes": page})
